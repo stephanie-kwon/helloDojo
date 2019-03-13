ed23104549d2007abc593b8a2782632b0c820d91
Stephanie <Stephaniemkwon@gmail.com>
2019-03-12 22:39:19 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// const show = React.createElement("h1", null,  "Hello Dojo!")
+// const sub = React.createElement("h3", null, "Things I need to do:")
+
+// ReactDOM.render(show, document.getElementById("showHeader"))
+// ReactDOM.render(sub, document.getElementById("subHeader"))
+
+ReactDOM.render("Hello Dojo!", document.getElementById("showHeader"))
+ReactDOM.render("Things I need to do:", document.getElementById("subHeader"))
+ReactDOM.render("Experience EDC", document.getElementById("item1"))
+ReactDOM.render("Travel the world", document.getElementById("item2"))
+ReactDOM.render("Buy another shiba", document.getElementById("item3"))
+
